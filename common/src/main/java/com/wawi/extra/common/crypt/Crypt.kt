0257748809d9c5a18ddd1348db23feb6a89d2a90
codeflow <1149863953@qq.com>
2019-07-12 10:08:49 +0800

    新增字符串MD5

--- common/src/main/java/com/wawi/extra/common/crypt/Crypt.kt
+++ common/src/main/java/com/wawi/extra/common/crypt/Crypt.kt
@@ -1,8 +1,38 @@
 package com.wawi.api.crypt
 
 import java.io.UnsupportedEncodingException
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
+
+/** Extensions */
+fun String.md5(): String {
+    try {
+        //获取md5加密对象
+        val instance: MessageDigest = MessageDigest.getInstance("MD5")
+        //对字符串加密，返回字节数组
+        val digest:ByteArray = instance.digest(this.toByteArray())
+        var sb : StringBuffer = StringBuffer()
+        for (b in digest) {
+            //获取低八位有效值
+            var i :Int = b.toInt() and 0xff
+            //将整数转化为16进制
+            var hexString = Integer.toHexString(i)
+            if (hexString.length < 2) {
+                //如果是一位的话，补0
+                hexString = "0$hexString"
+            }
+            sb.append(hexString)
+        }
+        return sb.toString()
+
+    } catch (e: NoSuchAlgorithmException) {
+        e.printStackTrace()
+    }
+
+    return ""
+}
 
 private const val AES_KEY = "bec2aks8bnhs2io4"
 
